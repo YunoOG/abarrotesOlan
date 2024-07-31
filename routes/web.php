1af73d3d6e74194542579bf5a2e3a02c164900e4
YunoOG <emilyjuliett13@gmail.com>
2024-07-30 20:14:42 -0600

    rutas corregidas

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
+Route::get('/admin', function () {
     return view('admin');
 });
 
